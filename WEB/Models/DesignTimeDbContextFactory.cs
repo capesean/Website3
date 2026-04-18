84d73451fc9e68e2b353e16d0f3f80132c0551d7
capesean <seanmatthewwalsh@gmail.com>
2026-04-18 12:18:34 +0200

    add design time db

---
+++ WEB/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Website3.Web.Models
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("DefaultConnection not found.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString, sqlOptions =>
+            {
+                sqlOptions.CommandTimeout(300);
+                sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+            });
+
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseOpenIddict();
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
